de0b32a16f9fd55eca355adaf8aa0a878ed0eef0
Joseph Starr <starr5@pdx.edu>
2022-01-28 11:34:25 -0800

    service calls unit tests done

--- creospan-angular-trainging/src/app/person.service.spec.ts
+++ creospan-angular-trainging/src/app/person.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from "@angular/core/testing";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'; 
 import { PersonService } from "./person.service";
 import { environment } from "src/environments/environment";
+import { Person } from "./person";
+import { HttpResponse } from "@angular/common/http";
 
 describe("Person Service", () => {
     const serverUrl = environment.apiBaseUrl;
@@ -24,7 +26,7 @@ describe("Person Service", () => {
         httpMock.verify();
     });
 
-    it('should retrieve all people vie GET', () => {
+    it('should retrieve all people via GET', () => {
         const mockPeople = [
             {
                 id: "1",
@@ -57,5 +59,62 @@ describe("Person Service", () => {
 
         request.flush(mockPeople);
 
+    });
+
+    it("should add person when POST is called", () => {
+        const mockPersonToAdd = {
+            id: "1",
+            firstName: "joseph",
+            lastName: "starr",
+            favoriteColor: "blue"
+        };
+
+        personService.addPerson(mockPersonToAdd).subscribe((person: Person) => {
+            expect(person).toEqual(mockPersonToAdd);
+        });
+
+        const request = httpMock.expectOne(`${serverUrl}/add-person`);
+
+        expect(request.request.method).toBe('POST');
+        expect(request.request.body).toEqual(mockPersonToAdd);
+
+        const expectedResponse = new HttpResponse({ status: 201, statusText: 'Success', body: mockPersonToAdd });
+        request.event(expectedResponse);
+
+        request.flush(mockPersonToAdd);
+    });
+
+    it("should modify person when PUT is called", () => {
+        const mockPersonToModify = {
+            id: "1",
+            firstName: "joseph",
+            lastName: "starr",
+            favoriteColor: "blue"
+        };
+
+        personService.modifyPerson(mockPersonToModify).subscribe((person: Person) => {
+            expect(person).toEqual(mockPersonToModify);
+        });
+
+        const request = httpMock.expectOne(`${serverUrl}/modify-person`);
+        expect(request.request.method).toEqual("PUT");
+        expect(request.request.body).toEqual(mockPersonToModify);
+
+        const expectedResponse = new HttpResponse({ status: 201, statusText: 'Success', body: mockPersonToModify });
+        request.event(expectedResponse);
+
+        request.flush(mockPersonToModify);
+    });
+
+    it("should delete person when DELETE is called", async () => {
+        const personId = "1";
+
+        personService.deletePerson("1").subscribe(response => {
+            expect(response).not.toBeNull();
+        });
+
+        const request = httpMock.expectOne(`${serverUrl}/delete-person/${personId}`);
+
+        expect(request.request.method).toBe("DELETE");
     });
 });
